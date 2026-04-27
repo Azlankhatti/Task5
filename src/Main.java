b73c0d6f94adfafd8de1bf9a7b2d4dbe9658ab16
Azlan Khatti <azalankhatti786@gmail.com>
2026-04-27 19:41:21 +0500

    Exception Handling

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,43 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+class InsufficientBalanceException extends Exception {
+    public InsufficientBalanceException(String message) {
+        super(message);
+    }
+}
+
+
+class BankAccount {
+    double balance;
+
+    BankAccount(double balance) {
+        this.balance = balance;
+    }
+
+
+    void withdraw(double amount) throws InsufficientBalanceException {
+        if (amount > balance) {
+            throw new InsufficientBalanceException("Error: Not enough balance!");
+        } else {
+            balance -= amount;
+            System.out.println("Withdraw successful. Remaining balance: " + balance);
+        }
+    }
+}
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        BankAccount account = new BankAccount(5000);
+
+        try {
+            account.withdraw(2000);  //ok
+            account.withdraw(4000);  // Error
+        }
+        catch (InsufficientBalanceException e) {
+            System.out.println(e.getMessage());
+        }
+        finally {
+            System.out.println("Transaction completed.");
         }
     }
 }
